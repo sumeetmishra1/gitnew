5882ea80b47fb2c1f4d9e3a65910f4ac2e75cefc
sumeetmishra1 <sumitsfs0@gmail.com>
2023-10-21 15:18:24 +0530

    js added

---
+++ dom.js
@@ -0,0 +1,14 @@
+//console.dir(document)
+//console.log(document.URL);
+//console.log(document.all)
+//document.all[20].textContent='sumit';
+
+//console.log(document.getElementById('header-title'));
+var headtitle =document.getElementById('header-title');
+//headtitle.textContent = "hello";
+//eadtitle.innerHTML ='Goodbye';
+var header =document.getElementById('main-header');
+header.style.borderBlock='solid 3px #000';
+var items =document.getElementById('title');
+items.style.fontWeight='bold';
+items.style.color='green';
