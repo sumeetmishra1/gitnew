288b108a65b73bb8e8c120de674bd73d9d38a8ce
sumeetmishra1 <sumitsfs0@gmail.com>
2023-10-21 17:41:38 +0530

    till query selectors

--- dom.js
+++ dom.js
@@ -20,10 +20,31 @@ var headtitle =document.getElementById('header-title');
 //     items[index].style.fontWeight='bold';
 // }
 // items[2].style.backgroundColor='green';
-var li =document.getElementsByTagName('li');
-li[1].textContent ='hello';
- for (let index = 0; index < li.length; index++) {
-    li[index].style.backgroundColor='yellow';
-    li[index].style.fontWeight='bold';
- }
- 
+// var li =document.getElementsByTagName('li');
+// li[1].textContent ='hello';
+//  for (let index = 0; index < li.length; index++) {
+//     li[index].style.backgroundColor='yellow';
+//     li[index].style.fontWeight='bold';
+//  }
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom='solid 4px #ccc';
+// var input =document.querySelector('input[type="submit"]');
+// input.value='hello world';
+// var item=document.querySelector('.list-group-item');
+// item.style.color='red';
+// var secitem=document.querySelector('.list-group-item:nth-child(2)');
+// secitem.style.color='green';
+// var thirdItem =document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.display='none';
+//Query selector all
+var titles=document.querySelectorAll('.title');
+console.log(titles);
+var items=document.querySelectorAll('li');
+items[1].style.color='green';
+titles[0].textContent='Add more items';
+var odd =document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='#ccc';
+}
+
+
