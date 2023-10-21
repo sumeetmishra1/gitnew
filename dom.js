ab475d7a64f4677fed9f12667706a81c811d16c1
sumeetmishra1 <sumitsfs0@gmail.com>
2023-10-21 16:57:19 +0530

    modified till tagname

--- dom.js
+++ dom.js
@@ -7,17 +7,23 @@
 var headtitle =document.getElementById('header-title');
 //headtitle.textContent = "hello";
 //eadtitle.innerHTML ='Goodbye';
-var header =document.getElementById('main-header');
-header.style.borderBlock='solid 3px #000';
-var items =document.getElementById('title');
-items.style.fontWeight='bold';
-items.style.color='green';
-var items =document.getElementsByClassName('list-group-item');
-console.log(items[1]);
-items[1].textContent ='hello';
-//
-for (let index = 0; index < items.length; index++) {
-    items[index].style.backgroundColor='yellow';
-    items[index].style.fontWeight='bold';
-}
-items[2].style.backgroundColor='green';
+// var header =document.getElementById('main-header');
+// header.style.borderBlock='solid 3px #000';
+// var items =document.getElementById('title');
+// items.style.fontWeight='bold';
+// items.style.color='green';
+// var items =document.getElementsByClassName('list-group-item');
+// console.log(items[1]);
+// items[1].textContent ='hello';
+// for (let index = 0; index < items.length; index++) {
+//     items[index].style.backgroundColor='yellow';
+//     items[index].style.fontWeight='bold';
+// }
+// items[2].style.backgroundColor='green';
+var li =document.getElementsByTagName('li');
+li[1].textContent ='hello';
+ for (let index = 0; index < li.length; index++) {
+    li[index].style.backgroundColor='yellow';
+    li[index].style.fontWeight='bold';
+ }
+ 
